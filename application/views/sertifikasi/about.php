68cd004298c6389dc6f22e8dc4d5ccc4648e8645
DhimasKrisna <dhimaskrisna@gmail.com>
2021-08-29 00:12:20 +0900

    sertifikasi

---
+++ application/views/sertifikasi/about.php
@@ -0,0 +1,35 @@
+
+        <div class="col-md-10 mt-5">
+          <h2>ABOUT</h2>
+          
+          <div class="row mt-3">
+            <div class="col-md-2">
+              <img src="<?= base_url()?>/assets/img/photo.JPG" width="200px" length="500px" alt="">
+            </div>
+            <div class="col-md-8">
+              <h4>Aplikasi ini dibuat oleh : </h4>
+              <h4>Nama : Dhimas Krisna Ahmadi</h4>
+              <h4>NIM : 1831710132</h4>
+              <h4>Tanggal : 28 Agustus 2021</h4>
+            </div>
+          </div>
+          
+
+          
+          
+          
+
+            
+
+            
+
+<!-- kene ntek -->
+        </div>
+        
+      </div>
+    </div>
+
+    
+
+
+
